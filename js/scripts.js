50e40aa7e0d5990a7e99461c55d00a56a83013de
BartPu <bpuchalski@tlen.pl>
2017-08-09 20:18:55 +0200

    i go it

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.',
-    dinosaur = 'triceratops',
-    dinosaurUpperCased = dinosaur.toUpperCase();
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+var dinosaurUpperCased = dinosaur.toUpperCase();
 console.log(dinosaurUpperCased);
 
-var textAfter = text.replace('Velociraptor', (dinosaurUpperCased));
-console.log(textAfter.length / 2);
-console.log(textAfter.substr(0, 72));
+var textAfter = text.replace('Velociraptor', dinosaurUpperCased);
+console.log(textAfter.substr(0, (textAfter.length / 2)));
